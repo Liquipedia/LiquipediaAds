22585b320bf5f5437a8cd95c3a443e7f44a5f893
FO-nTTaX <fo.nttax@yahoo.de>
2017-05-10 19:24:47 +0200

    fix a bug that had headers not appearing

--- LiquiFlowAdsHooks.php
+++ LiquiFlowAdsHooks.php
@@ -38,7 +38,6 @@ END_HTML;
 	}
 	public static function onParserBeforeStrip( &$parser, &$text, &$mStripState ) {
 		$adbox_tag = self::getAdboxTag();
-		$$adbox_tag = "\n" . $adbox_tag . "\n";
 		$title = $parser->getTitle();
 		if($title->getNamespace() != NS_MAIN) {
 			return true;
@@ -54,17 +53,15 @@ END_HTML;
 			return;
 		}
 		$has_added_adbox = false;
-		if (preg_match_all("/\n\s*==([^=]+)==\s*\n/", "\n" . $text, $findings)) {
+		if (preg_match_all("/\s*==([^=]+)==\s*/", "\n" . $text, $findings)) {
 			//$number_of_adboxes = 1;
 			$pages = wfMessage( 'adbox-headings' )->plain();
 			$key_headings = explode("\n", $pages);
-			//echo '<pre>'.print_r(str_replace("\n","NNN",$text),true).'</pre>';
 			foreach($key_headings as $key_heading) {
 				foreach($findings[1] as $findingid => $finding) {
 					if(!$has_added_adbox) {
-						$pos = strpos($text, $findings[0][$findingid]);
 						if (trim($finding) == trim($key_heading, "* \t\n\r\0\x0B")) {
-							$text = substr_replace($text, $findings[0][$findingid] . $adbox_tag, $pos, strlen($findings[0][$findingid]));
+							$text = preg_replace('/' . preg_quote($findings[0][$findingid]) . '/', $findings[0][$findingid] . "\n" . $adbox_tag . "\n", $text);
 							$has_added_adbox = true;
 							break 2;
 						}
@@ -74,10 +71,9 @@ END_HTML;
 		}
 		if(!$has_added_adbox) {
 			if (count($findings[0]) <= 2) {
-				$text = $text . $adbox_tag;
+				$text = $text . "\n" . $adbox_tag;
 			} else {
-				$pos = strpos($text, $findings[0][ceil((count($findings[0]) - 1) / 2)]);
-				$text = substr_replace($text, $findings[0][ceil((count($findings[0]) - 1) / 2)] . $adbox_tag, $pos, strlen($findings[0][ceil((count($findings[0]) - 1) / 2)]));
+				$text = preg_replace('/' . preg_quote($findings[0][ceil((count($findings[0]) - 1) / 2)]) . '/', $findings[0][ceil((count($findings[0]) - 1) / 2)] . "\n" . $adbox_tag . "\n", $text);
 				$has_added_adbox = true;
 			}
 		}
