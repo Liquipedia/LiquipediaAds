4623a291958d09e952667f41046027ba3cb2049a
FO-nTTaX <fo.nttax@yahoo.de>
2017-05-11 21:54:34 +0200

    php is weird

--- LiquiFlowAdsHooks.php
+++ LiquiFlowAdsHooks.php
@@ -61,7 +61,7 @@ END_HTML;
 				foreach($findings[1] as $findingid => $finding) {
 					if(!$has_added_adbox) {
 						if (trim($finding) == trim($key_heading, "* \t\n\r\0\x0B")) {
-							$text = preg_replace('/' . preg_quote($findings[0][$findingid]) . '/', $findings[0][$findingid] . "\n" . $adbox_tag . "\n", $text);
+							$text = preg_replace('/' . str_replace('/', '\/', preg_quote($findings[0][$findingid])) . '/', $findings[0][$findingid] . "\n" . $adbox_tag . "\n", $text);
 							$has_added_adbox = true;
 							break 2;
 						}
@@ -73,7 +73,7 @@ END_HTML;
 			if (count($findings[0]) <= 2) {
 				$text = $text . "\n" . $adbox_tag;
 			} else {
-				$text = preg_replace('/' . preg_quote($findings[0][ceil((count($findings[0]) - 1) / 2)]) . '/', $findings[0][ceil((count($findings[0]) - 1) / 2)] . "\n" . $adbox_tag . "\n", $text);
+				$text = preg_replace('/' . str_replace('/', '\/', preg_quote($findings[0][ceil((count($findings[0]) - 1) / 2)])) . '/', $findings[0][ceil((count($findings[0]) - 1) / 2)] . "\n" . $adbox_tag . "\n", $text);
 				$has_added_adbox = true;
 			}
 		}
