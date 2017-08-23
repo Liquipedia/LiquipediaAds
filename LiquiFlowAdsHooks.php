56690fce80880faadc8871fd3f5462eba9633f63
Richard Stanway <r1ch@teamliquid.net>
2017-08-24 04:45:54 +0900

    Updated ad code

--- LiquiFlowAdsHooks.php
+++ LiquiFlowAdsHooks.php
@@ -20,15 +20,39 @@ class LiquiFlowAdsHooks {
 		return true;
 	}
 	public static function onLiquiFlowAdStartCode(OutputPage $out) {
-		$tlAdCode = <<<END_HTML
+		global $liquipedia_ads;
+		if ($liquipedia_ads['adonis_v2'])
+		{
+			$tlAdCode = <<<END_HTML
+<script>!function t(e,n,o){function r(a,s){if(!n[a]){if(!e[a]){var u="function"==typeof require&&require;if(!s&&u)return u(a,!0);if(i)return i(a,!0);var d=new Error("Cannot find module '"+a+"'");throw d.code="MODULE_NOT_FOUND",d}var c=n[a]={exports:{}};e[a][0].call(c.exports,function(t){var n=e[a][1][t];return r(n||t)},c,c.exports,t,e,n,o)}return n[a].exports}for(var i="function"==typeof require&&require,a=0;a<o.length;a++)r(o[a]);return r}({1:[function(t,e,n){"use strict";function o(t){var e=arguments.length>1&&void 0!==arguments[1]?arguments[1]:"NEW_SCRIPT_EVENT",n=arguments.length>2&&void 0!==arguments[2]?arguments[2]:{},o=new r(e,{detail:n});t.dispatchEvent(o)}var r=t(3);e.exports=o},{3:3}],2:[function(t,e,n){"use strict";function o(t){t.adonis=t.adonis||{},t.adonis.scriptStatuses={},t.adonis.requestStatuses={};var e=t.XMLHttpRequest.prototype.open;t.XMLHttpRequest.prototype.open=function(n,o){this.addEventListener("error",function(e){0===this.status?t.adonis.scriptStatuses[o]="error":t.adonis.scriptStatuses[o]="load"}),this.addEventListener("load",function(e){t.adonis.scriptStatuses[o]="load"}),this.addEventListener("loadend",function(e){t.adonis.requestStatuses[o]=this.status,r(t)});var i=[].slice.call(arguments,0);return e.apply(this,i)}}var r=t(1);e.exports={wrapXMLHttpRequest:o}},{1:1}],3:[function(t,e,n){(function(t){var n=t.CustomEvent;e.exports=function(){try{var t=new n("cat",{detail:{foo:"bar"}});return"cat"===t.type&&"bar"===t.detail.foo}catch(t){}return!1}()?n:"undefined"!=typeof document&&"function"==typeof document.createEvent?function(t,e){var n=document.createEvent("CustomEvent");return e?n.initCustomEvent(t,e.bubbles,e.cancelable,e.detail):n.initCustomEvent(t,!1,!1,void 0),n}:function(t,e){var n=document.createEventObject();return n.type=t,e?(n.bubbles=Boolean(e.bubbles),n.cancelable=Boolean(e.cancelable),n.detail=e.detail):(n.bubbles=!1,n.cancelable=!1,n.detail=void 0),n}}).call(this,"undefined"!=typeof global?global:"undefined"!=typeof self?self:"undefined"!=typeof window?window:{})},{}],4:[function(t,e,n){"use strict";function o(t,e,n,o){"addEventListener"in t?t.addEventListener(e,n,o):"attachEvent"in t&&t.attachEvent("on"+e,n)}var r=t(2),i=t(1);!function(){window.adonis=window.adonis||{},window.adonis.scriptStatuses={},r.wrapXMLHttpRequest(window),o(document,"load",function(t){t.target&&"SCRIPT"===t.target.nodeName&&(window.adonis.scriptStatuses[t.target.src]="load",i(window))},!0),o(document,"error",function(t){t.target&&"SCRIPT"===t.target.nodeName&&(window.adonis.scriptStatuses[t.target.src]="error",i(window))},!0)}()},{1:1,2:2}]},{},[4]);var adonis = adonis || {};adonis.conditionalAdRendering = true;adonis.transport = 'https://www.adiode.com/http';</script>
+		<script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
+		<script>
+			var googletag = googletag || {};
+			googletag.cmd = googletag.cmd || [];
+			var advelvetTargeting = [];
+			advelvetTargeting.push((Math.floor(Math.random() * 20) + 1) + "");
+			googletag.cmd.push(function () {
+				googletag.pubads().setTargeting('advelvet', advelvetTargeting);
+			});
+		</script>
+END_HTML;
+		}
+		else
+		{
+			$tlAdCode = <<<END_HTML
 <script>/* observer-6.0.10 */!function t(e,n,o){function r(i,s){if(!n[i]){if(!e[i]){var u="function"==typeof require&&require;if(!s&&u)return u(i,!0);if(a)return a(i,!0);var c=new Error("Cannot find module '"+i+"'");throw c.code="MODULE_NOT_FOUND",c}var d=n[i]={exports:{}};e[i][0].call(d.exports,function(t){var n=e[i][1][t];return r(n?n:t)},d,d.exports,t,e,n,o)}return n[i].exports}for(var a="function"==typeof require&&require,i=0;i<o.length;i++)r(o[i]);return r}({1:[function(t,e,n){function o(t){var e="NEW_SCRIPT_EVENT",n=new r(e,{detail:{}});t.dispatchEvent(n)}const r=t(3);e.exports=o},{3:3}],2:[function(t,e,n){function o(t){t.adonis=t.adonis||{},t.adonis.scriptStatuses={},t.adonis.requestStatuses={};const e=t.XMLHttpRequest,n=function(){const n=new e,o=n.open;return n.open=function(e,a){return n.addEventListener("error",function(e){0===n.status?t.adonis.scriptStatuses[a]="error":t.adonis.scriptStatuses[a]="load"}),n.addEventListener("load",function(e){t.adonis.scriptStatuses[a]="load"}),n.addEventListener("loadend",function(e){t.adonis.requestStatuses[a]=n.status,r(t)}),o.apply(n,arguments)},n};t.XMLHttpRequest=n,e.prototype.constructor=n}const r=t(1);e.exports={wrapXMLHttpRequest:o}},{1:1}],3:[function(t,e,n){(function(t){function n(){try{var t=new o("cat",{detail:{foo:"bar"}});return"cat"===t.type&&"bar"===t.detail.foo}catch(e){}return!1}var o=t.CustomEvent;e.exports=n()?o:"undefined"!=typeof document&&"function"==typeof document.createEvent?function(t,e){var n=document.createEvent("CustomEvent");return e?n.initCustomEvent(t,e.bubbles,e.cancelable,e.detail):n.initCustomEvent(t,!1,!1,void 0),n}:function(t,e){var n=document.createEventObject();return n.type=t,e?(n.bubbles=Boolean(e.bubbles),n.cancelable=Boolean(e.cancelable),n.detail=e.detail):(n.bubbles=!1,n.cancelable=!1,n.detail=void 0),n}}).call(this,"undefined"!=typeof global?global:"undefined"!=typeof self?self:"undefined"!=typeof window?window:{})},{}],4:[function(t,e,n){function o(t,e,n,o){"addEventListener"in t?t.addEventListener(e,n,o):"attachEvent"in t&&t.attachEvent("on"+e,n)}const r=t(2),a=t(1);!function(){window.adonis=window.adonis||{},window.adonis.scriptStatuses={},r.wrapXMLHttpRequest(window),o(document,"load",function(t){t.target&&"SCRIPT"===t.target.nodeName&&(window.adonis.scriptStatuses[t.target.src]="load",a(window))},!0),o(document,"error",function(t){t.target&&"SCRIPT"===t.target.nodeName&&(window.adonis.scriptStatuses[t.target.src]="error",a(window))},!0)}()},{1:1,2:2}]},{},[4]);</script>
 		<script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
 		<script>
-		  var googletag = googletag || {};
-		  googletag.cmd = googletag.cmd || [];
+			var googletag = googletag || {};
+			googletag.cmd = googletag.cmd || [];
+			var advelvetTargeting = [];
+			advelvetTargeting.push((Math.floor(Math.random() * 20) + 1) + "");
+			googletag.cmd.push(function () {
+				googletag.pubads().setTargeting('advelvet', advelvetTargeting);
+			});
 		</script>
 END_HTML;
-		global $liquipedia_ads;
+		}
 		$out->addHeadItem( 'tlads', $tlAdCode . $liquipedia_ads['header']);
 		return true;
 	}
